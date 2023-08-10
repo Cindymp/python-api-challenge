d8572b426e22c804c0ca75b745298ab038fc8dd7
Cindymp <135760131+Cindymp@users.noreply.github.com>
2023-08-10 17:54:13 -0400

    my last commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e8efdac90d49546a4264050f6bad5795"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "cb9f0382070540ce876547e08713f6c9"
